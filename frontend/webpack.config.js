666950cadc64d84d58239352e91693e945dca5c2
Kardkasem Saenyen <meoss@Kardkasems-MacBook-Air.local>
2019-11-26 18:09:01 +1100

    add mern_form url

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-react"]
+            presets: ["@babel/react", "@babel/preset-env"]
           }
         }
       }
